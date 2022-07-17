b07907c6ceca7846443216ba9c51a7ae05cff918
Andy Nguyen <andynguyen_3@hotmail.com>
2022-07-17 18:21:38 +1000

    portfolio js added

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,1 +1,27 @@
 import React from 'react';
+import Project from '../components/Project';
+import projects from '../projects.json';
+
+function Portfolio () {
+    return (
+        <section>
+            <h1>Portfolio</h1>
+            <div className='row'>
+                {projects.map((projects) => {
+                    <Project 
+                        projects={projects}
+                        key={projects.id}
+                        title={projects.title}
+                        description={projects.description}
+                        githubLink={projects.githubLink}
+                        deployedLink={projects.deployedLink}
+                        >
+                    </Project>
+                })}
+            </div>
+        </section>
+    )
+}
+
+export default Portfolio;
+
